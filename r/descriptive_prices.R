d8858033b43014554de61c3800207892295a8837
Till Grallert <till.grallert@fu-berlin.de>
2017-07-12 15:46:33 +0200

    edited function to select a period

--- r/descriptive_prices.R
+++ r/descriptive_prices.R
@@ -6,6 +6,9 @@ library(scales)   # to access breaks/formatting functions
 library(gridExtra) # for arranging plots
 library(plotly) # interactive plots based on ggplot
 
+# function to create subsets for periods
+func_period <- function(f,x,y){f[f$date >= x & f$date <= y,]}
+
 # use a working directory
 setwd("/Volumes/Dessau HD/BachCloud/BTSync/FormerDropbox/PostDoc Food Riots/food-riots_data")
 
@@ -17,19 +20,17 @@ v_pricesWheat <- read.csv("csv/prices_wheat-kile.csv", header=TRUE, sep = ",")
 v_pricesBread <- read.csv("csv/prices_bread-kg.csv", header=TRUE, sep = ",") # this file throws an error
 
 # convert date to Date class
-v_prices$date <- as.Date(v_prices$date)
+# v_prices$date <- as.Date(v_prices$date)
 v_pricesWheat$date <- as.Date(v_pricesWheat$date)
 
 # create a subset of rows based on conditions
 v_wheatKile <- subset(v_pricesWheat,commodity.1=="wheat" & unit.1=="kile" & commodity.2=="currency" & unit.2=="ops")
 v_barleyKile <- subset(v_prices,commodity.1=="barley" & unit.1=="kile")
 
-## create subsets for periods
-func_period <- function(x,y){v_pricesWheat[v_pricesWheat$date >= x & v_pricesWheat$date <= y,]}
 ## specify period
-v_dateStart <- as.Date("1908-01-01")
+v_dateStart <- as.Date("1875-01-01")
 v_dateStop <- as.Date("1916-12-31")
-v_wheatKilePeriod <- func_period(v_dateStart,v_dateStop) 
+v_wheatKilePeriod <- func_period(v_wheatKile,v_dateStart,v_dateStop) 
 
 # select rows
 ## select the first row (containing dates), and the rows containing prices in ops
