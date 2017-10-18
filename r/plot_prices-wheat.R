3f9e68897f56fe5e8d3c7f8ad360484ee3764462
Till Grallert <till.grallert@fu-berlin.de>
2017-10-18 12:04:38 +0300

    cleaned the code for better readibility

--- r/plot_prices-wheat.R
+++ r/plot_prices-wheat.R
@@ -32,7 +32,7 @@ vPricesWheat$week <- as.Date(cut(vPricesWheat$date,
                                 start.on.monday = TRUE)) # allows to change weekly break point to Sunday
 
 # create a subset of rows based on conditions
-vWheatKile <- subset(vpricesWheat,commodity.1=="wheat" & unit.1=="kile" & commodity.2=="currency" & unit.2=="ops")
+vWheatKile <- subset(vPricesWheat,commodity.1=="wheat" & unit.1=="kile" & commodity.2=="currency" & unit.2=="ops")
 
 # select rows
 ## select the first row (containing dates), and the rows containing prices in ops
@@ -45,26 +45,52 @@ vWheatKileSimple <- vwheatKile[,c("date","quantity.2","quantity.3")]
 
 ## plot only for period
 ## specify period
-vDateStart <- as.Date("1909-07-01")
-vDateStop <- as.Date("1910-12-31")
-vWheatKilePeriod <- funcPeriod(vWheatKile,vdateStart,vdateStop)  
+vDateStart <- as.Date("1875-01-01")
+vDateStop <- as.Date("1916-12-31")
+vWheatKilePeriod <- funcPeriod(vWheatKile,vDateStart,vDateStop)  
 
 ## plot
-plotWheatKilePeriod1 <- ggplot(vWheatKilePeriod, aes(date,quantity.2, quantity.3)) +
-  ggtitle("Wheat prices in Bilad al-Sham") +
-  xlab("Date") + ylab("Prices (piaster/kile)") +
-  geom_point(na.rm=TRUE, color="purple", size=1, pch=3) +
-  scale_x_date(breaks=date_breaks("1 years"), labels=date_format("%Y")) +
-  stat_smooth(colour="green", method="loess") +
+plotWheatKilePeriod1 <- ggplot(vWheatKilePeriod, 
+                               aes(date,
+                                   quantity.2, quantity.3)) +
+  # add labels
+  labs(title="Wheat prices in Bilad al-Sham", 
+       # subtitle="based on announcements in newspapers", 
+       x="Date", 
+       y="Prices (piaster/kile)") + # provides title, subtitle, x, y, caption
+  # first layer: all prices
+  geom_point(na.rm=TRUE, 
+             color="purple", 
+             size=1, 
+             pch=3) +
+  # second layer: fitted line
+  stat_smooth(colour="green",
+              na.rm = TRUE,
+              method="loess") +
+  scale_x_date(breaks=date_breaks("5 years"), 
+               labels=date_format("%Y"),
+               limits=as.Date(c(vDateStart, vDateStop))) + # if plotting more than one graph, it is helpful to provide the same limits for each
   theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
+plotWheatKilePeriod1
   
 ## plot with two time series
 plotWheatKilePeriod2 <- ggplot(vWheatKilePeriod, aes(x=date, y=value)) +
-  ggtitle("Wheat prices in Bilad al-Sham") +
-  xlab("Date") + ylab("Prices (piaster/kile)") +
-  geom_point(aes(y=quantity.2, col='min price'), na.rm=TRUE, size=2, pch=1, color="black")  +
-  geom_point(aes(y=quantity.3, col='max price'), na.rm=TRUE, size=2, pch=3, color="black") +
-  scale_x_date(breaks=date_breaks("2 years"), labels=date_format("%Y")) + # add interval to x-axis
+  # add labels
+  labs(title="Wheat prices in Bilad al-Sham", 
+       # subtitle="based on announcements in newspapers", 
+       x="Date", 
+       y="Prices (piaster/kile)") + # provides title, subtitle, x, y, caption
+  # first layer: min prices
+  geom_point(aes(y=quantity.2, col='min price'),
+             na.rm=TRUE,
+             size=2, pch=1, color="black")  +
+  # second layer: max prices
+  geom_point(aes(y=quantity.3, col='max price'),
+             na.rm=TRUE, 
+             size=2, pch=3, color="black") +
+  scale_x_date(breaks=date_breaks("5 years"), 
+               labels=date_format("%Y"),
+               limits=as.Date(c(vDateStart, vDateStop))) + # if plotting more than one graph, it is helpful to provide the same limits for each
   theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
 ## final plot
 plotWheatKilePeriod1
