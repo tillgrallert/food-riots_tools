182871c76a6730169d4470055d00609a28de11b2
Till Grallert <till.grallert@fu-berlin.de>
2017-10-18 11:56:14 +0300

    changed names and added aggregation by periods

--- r/plot_prices-wheat.R
+++ r/plot_prices-wheat.R
@@ -13,31 +13,44 @@ funcPeriod <- function(f,x,y){f[f$date >= x & f$date <= y,]}
 setwd("/Volumes/Dessau HD/BachCloud/BTSync/FormerDropbox/FoodRiots/food-riots_data")
 
 # 1. read price data from csv, note that the first row is a date
-v_pricesWheat <- read.csv("csv/prices_wheat-kile.csv", header=TRUE, sep = ",", quote = "")
+vPricesWheat <- read.csv("csv/prices_wheat-kile.csv", header=TRUE, sep = ",", quote = "")
 
 # convert date to Date class
-v_pricesWheat$date <- as.Date(v_pricesWheat$date)
+vPricesWheat$date <- as.Date(vPricesWheat$date)
+
+# aggregate periods
+## use cut() to generate summary stats for time periods
+## create variables of the year, quarter week and month of each observation:
+vPricesWheat$year <- as.Date(cut(vPricesWheat$date,
+                                breaks = "year"))
+vPricesWheat$quarter <- as.Date(cut(vPricesWheat$date,
+                                   breaks = "quarter"))
+vPricesWheat$month <- as.Date(cut(vPricesWheat$date,
+                                 breaks = "month"))
+vPricesWheat$week <- as.Date(cut(vPricesWheat$date,
+                                breaks = "week",
+                                start.on.monday = TRUE)) # allows to change weekly break point to Sunday
 
 # create a subset of rows based on conditions
-v_wheatKile <- subset(v_pricesWheat,commodity.1=="wheat" & unit.1=="kile" & commodity.2=="currency" & unit.2=="ops")
+vWheatKile <- subset(vpricesWheat,commodity.1=="wheat" & unit.1=="kile" & commodity.2=="currency" & unit.2=="ops")
 
 # select rows
 ## select the first row (containing dates), and the rows containing prices in ops
-## v_wheatKileSimple <- v_wheatKile[,c(1,8,11)]
-v_wheatKileSimple <- v_wheatKile[,c("date","quantity.2","quantity.3")]
+## vwheatKileSimple <- vwheatKile[,c(1,8,11)]
+vWheatKileSimple <- vwheatKile[,c("date","quantity.2","quantity.3")]
 
 
 # 2. plot with ggplot
-## plot_wheatKile <- ggplot(v_wheatKileSimple, aes(date,quantity.2, quantity.3)) + ggtitle("Wheat prices in Bilad al-Sham") + xlab("Date") + ylab("Prices (piaster)") + geom_point(na.rm=TRUE, color="purple", size=3, pch=1)
+## plot_wheatKile <- ggplot(vwheatKileSimple, aes(date,quantity.2, quantity.3)) + ggtitle("Wheat prices in Bilad al-Sham") + xlab("Date") + ylab("Prices (piaster)") + geom_point(na.rm=TRUE, color="purple", size=3, pch=1)
 
 ## plot only for period
 ## specify period
-v_dateStart <- as.Date("1909-07-01")
-v_dateStop <- as.Date("1910-12-31")
-v_wheatKilePeriod <- func_period(v_wheatKileSimple,v_dateStart,v_dateStop)  
+vDateStart <- as.Date("1909-07-01")
+vDateStop <- as.Date("1910-12-31")
+vWheatKilePeriod <- funcPeriod(vWheatKile,vdateStart,vdateStop)  
 
 ## plot
-plot_wheatKilePeriod1 <- ggplot(v_wheatKilePeriod, aes(date,quantity.2, quantity.3)) +
+plotWheatKilePeriod1 <- ggplot(vWheatKilePeriod, aes(date,quantity.2, quantity.3)) +
   ggtitle("Wheat prices in Bilad al-Sham") +
   xlab("Date") + ylab("Prices (piaster/kile)") +
   geom_point(na.rm=TRUE, color="purple", size=1, pch=3) +
@@ -46,7 +59,7 @@ plot_wheatKilePeriod1 <- ggplot(v_wheatKilePeriod, aes(date,quantity.2, quantity
   theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
   
 ## plot with two time series
-plot_wheatKilePeriod2 <- ggplot(v_wheatKilePeriod, aes(x=date, y=value)) +
+plotWheatKilePeriod2 <- ggplot(vWheatKilePeriod, aes(x=date, y=value)) +
   ggtitle("Wheat prices in Bilad al-Sham") +
   xlab("Date") + ylab("Prices (piaster/kile)") +
   geom_point(aes(y=quantity.2, col='min price'), na.rm=TRUE, size=2, pch=1, color="black")  +
@@ -54,5 +67,5 @@ plot_wheatKilePeriod2 <- ggplot(v_wheatKilePeriod, aes(x=date, y=value)) +
   scale_x_date(breaks=date_breaks("2 years"), labels=date_format("%Y")) + # add interval to x-axis
   theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
 ## final plot
-plot_wheatKilePeriod1
-plot_wheatKilePeriod2
+plotWheatKilePeriod1
+plotWheatKilePeriod2
