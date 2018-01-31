92c0a61a6ad39e5a358f98c7a5d20b53c21ac431
Till Grallert <till.grallert@fu-berlin.de>
2018-01-31 23:22:47 +0200

    added a new plot of vertical lines between min and max prices

--- r/plot_prices-wheat.R
+++ r/plot_prices-wheat.R
@@ -23,14 +23,10 @@ vFoodRiots$date <- as.Date(vFoodRiots$date)
 # aggregate periods
 ## use cut() to generate summary stats for time periods
 ## create variables of the year, quarter week and month of each observation:
-vPricesWheat$year <- as.Date(cut(vPricesWheat$date,
-                                breaks = "year"))
-vPricesWheat$quarter <- as.Date(cut(vPricesWheat$date,
-                                   breaks = "quarter"))
-vPricesWheat$month <- as.Date(cut(vPricesWheat$date,
-                                 breaks = "month"))
-vPricesWheat$week <- as.Date(cut(vPricesWheat$date,
-                                breaks = "week",
+vPricesWheat$year <- as.Date(cut(vPricesWheat$date, breaks = "year"))
+vPricesWheat$quarter <- as.Date(cut(vPricesWheat$date,breaks = "quarter"))
+vPricesWheat$month <- as.Date(cut(vPricesWheat$date,breaks = "month"))
+vPricesWheat$week <- as.Date(cut(vPricesWheat$date,breaks = "week",
                                 start.on.monday = TRUE)) # allows to change weekly break point to Sunday
 
 # create a subset of rows based on conditions
@@ -42,8 +38,8 @@ vWheatKile <- subset(vPricesWheat,commodity.1=="wheat" & unit.1=="kile" & commod
 vWheatKileSimple <- vWheatKile[,c("date","quantity.2","quantity.3")]
 
 # specify period
-vDateStart <- as.Date("1876-01-01")
-vDateStop <- as.Date("1885-12-31")
+vDateStart <- as.Date("1875-01-01")
+vDateStop <- as.Date("1916-12-31")
 vWheatKilePeriod <- funcPeriod(vWheatKile,vDateStart,vDateStop) 
 vFoodRiotsPeriod <- funcPeriod(vFoodRiots,vDateStart,vDateStop) 
 
@@ -143,36 +139,23 @@ plotLineAvgQuarterlyMin
 ## box plot
 plotBoxAnnualMin <- ggplot() +
   # add labels
-  labs(title="Wheat prices in Bilad al-Sham", 
-       subtitle="quarterly average minimum prices", 
+  labs(title="Wheat prices and food riots in Bilad al-Sham", 
+       subtitle="minimum prices aggregated by year", 
        x="Date", 
        y="Price (piaster/kile)") + # provides title, subtitle, x, y, caption
   # layer: vertical lines for bread riots
-  #geom_vline(data = vFoodRiotsPeriod,
-   #          aes(xintercept = as.numeric(as.Date(date))),
-    #        na.rm = T, linetype=2, # linetypes: 1=solid, 2=dashed, 
-     #      size=1, color = "#981103")+
-  geom_segment(data = vFoodRiotsPeriod, 
-               aes(x = date, y = 10, 
-                   xend = date, yend = 24,
-                   colour = "food riot"),
-               na.rm = T, linetype=1, # linetypes: 1=solid, 2=dashed, 
-               size=1,
-               show.legend = T)+
+  #geom_vline(data = vFoodRiotsPeriod,aes(xintercept = as.numeric(as.Date(date))),
+   #          size=1, color = "#981103", na.rm = T, linetype=2)+ # linetypes: 1=solid, 2=dashed, 
+  geom_segment(data = vFoodRiotsPeriod, aes(x = date, xend = date, y = 10, yend = 24, colour = "food riot"),
+               size = 1, show.legend = F, na.rm = T, linetype=1)+ # linetypes: 1=solid, 2=dashed, 
+  # layer: box plot prices, average of min and max prices
+  #geom_boxplot(data = vWheatKilePeriod,aes(x=year,group=year,y=(quantity.2 + quantity.3) / 2), na.rm = T)+
   # layer: box plot min prices
-  geom_boxplot(data = vWheatKilePeriod,
-               aes(x=year,
-                   group=year,
-                   y=quantity.2), na.rm = T)+
+  geom_boxplot(data = vWheatKilePeriod,aes(x=year,group=year,y=quantity.2), na.rm = T)+
   # layer: box plot max prices
-  #geom_boxplot(aes(x=year, group=year,y=quantity.3), na.rm = T, color="blue", width=50)+
+  #geom_boxplot(data = vWheatKilePeriod,aes(x=year, group=year,y=quantity.3), na.rm = T, color="blue", width=50)+
   # layer: jitter plot
-  geom_jitter(data = vWheatKilePeriod,
-              aes(date, quantity.2,
-                  colour = "price points"), 
-              na.rm=TRUE,width = 50, # width depends on the width of the entire plot
-              size=1) +
-  #geom_jitter(aes(date, quantity.3), na.rm=TRUE,width = 100,size=1, color="red") +
+  #geom_jitter(data = vWheatKilePeriod,aes(date, quantity.2,colour = "price points"), size=1, na.rm=TRUE,width = 50)+ # width depends on the width of the entire plot
   # layer: line with all values
   #geom_line(aes(date, quantity.2), na.rm=TRUE,color="red") +
   # layer: fitted line
@@ -206,22 +189,57 @@ plotBoxAnnualMax <- ggplot(vWheatKilePeriod) +
 plotBoxAnnualMax
   
 ## plot with two time series
-plotWheatKilePeriod2 <- ggplot(vWheatKilePeriod, aes(x=year, y=value)) +
+plotWheatKilePeriod2 <- ggplot(vWheatKilePeriod) +
   # add labels
   labs(title="Wheat prices in Bilad al-Sham", 
        # subtitle="based on announcements in newspapers", 
        x="Date", 
        y="Prices (piaster/kile)") + # provides title, subtitle, x, y, caption
+  # layer: vertical lines for bread riots
+  geom_segment(data = vFoodRiotsPeriod, aes(x = date, xend = date, y = 10, yend = 24, colour = "food riot"),
+               size = 1, show.legend = F, na.rm = T, linetype=1)+ # linetypes: 1=solid, 2=dashed,
   # first layer: min prices
-  geom_point(aes(y=quantity.2),
+  geom_point(aes(x=date, y=quantity.2),
              na.rm=TRUE,
              size=2, pch=1, color="black")  +
   # second layer: max prices
-  geom_point(aes(y=quantity.3),
+  geom_point(aes(x=date, y=quantity.3),
              na.rm=TRUE, 
              size=2, pch=3, color="black") +
-  scale_x_date(breaks=date_breaks("5 years"), 
+  scale_x_date(breaks=date_breaks("2 years"), 
                labels=date_format("%Y"),
                limits=as.Date(c(vDateStart, vDateStop))) + # if plotting more than one graph, it is helpful to provide the same limits for each
-  theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
+  theme_bw()+ # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
+  theme(axis.text.x = element_text(angle = 45, vjust=0.5,hjust = 0.5, size = 8))  # rotate x axis text
 plotWheatKilePeriod2
+
+vDateStart <- as.Date("1908-01-01")
+vDateStop <- as.Date("1916-12-31")
+vWheatKilePeriod <- funcPeriod(vWheatKile,vDateStart,vDateStop) 
+vFoodRiotsPeriod <- funcPeriod(vFoodRiots,vDateStart,vDateStop) 
+
+plotWheatKilePeriod3 <- ggplot(vWheatKilePeriod) +
+  # add labels
+  labs(title="Wheat prices in Bilad al-Sham", 
+       # subtitle="based on announcements in newspapers", 
+       x="Date", 
+       y="Prices (piaster/kile)") + # provides title, subtitle, x, y, caption
+  # layer: vertical lines for bread riots
+  geom_segment(data = vFoodRiotsPeriod, aes(x = date, xend = date, y = 10, yend = 24, colour = "food riot"),
+               size = 1, show.legend = F, na.rm = T, linetype=1)+ # linetypes: 1=solid, 2=dashed, 
+  # first layer: min prices
+  geom_point(aes(x=date, y=quantity.2),
+             na.rm=TRUE,
+             size=2, pch=3, color="black")  +
+  # second layer: max prices
+  geom_point(aes(x=date, y=quantity.3),
+             na.rm=TRUE, 
+             size=2, pch=3, color="black") +
+  # layer with connecting lines between min and max prices
+  geom_segment(aes(x = date, xend = date, y = quantity.2, yend = quantity.3), show.legend = F, na.rm = T, linetype=1, color = "black")+ # linetypes: 1=solid, 2=dashed, 
+  scale_x_date(breaks=date_breaks("2 years"), 
+               labels=date_format("%Y"),
+               limits=as.Date(c(vDateStart, vDateStop))) + # if plotting more than one graph, it is helpful to provide the same limits for each
+  theme_bw()+ # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
+  theme(axis.text.x = element_text(angle = 45, vjust=0.5,hjust = 0.5, size = 8))  # rotate x axis text
+plotWheatKilePeriod3
