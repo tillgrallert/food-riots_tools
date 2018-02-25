ac7ccc4d5192a7698372c8bfa6b346567cc05a92
Till Grallert <till.grallert@fu-berlin.de>
2018-02-26 00:34:37 +0200

    added columns and plots for seasonal changes

--- r/plot_prices.R
+++ r/plot_prices.R
@@ -16,9 +16,13 @@ setwd("/BachCloud/BTSync/FormerDropbox/FoodRiots/food-riots_data") #Volumes/Dess
 vFoodRiots <- read.csv("csv/events_food-riots.csv", header=TRUE, sep = ",", quote = "")
 v.Prices <- read.csv("csv/prices.csv", header=TRUE, sep = ",", quote = "")
 
-# convert date to Date class
+# fix date types
+## convert date to Date class, note that dates supplied as years only will be turned into NA
 vFoodRiots$date <- as.Date(vFoodRiots$date)
 v.Prices$date <- as.Date(v.Prices$date)
+## numeric
+#v.Prices$quantity.2 <- as.numeric(v.Prices$quantity.2)
+#v.Prices$quantity.3 <- as.numeric(v.Prices$quantity.3)
 
 # aggregate periods
 ## use cut() to generate summary stats for time periods
@@ -28,6 +32,9 @@ v.Prices$quarter <- as.Date(cut(v.Prices$date,breaks = "quarter"))
 v.Prices$month <- as.Date(cut(v.Prices$date,breaks = "month"))
 v.Prices$week <- as.Date(cut(v.Prices$date,breaks = "week",
                                  start.on.monday = TRUE)) # allows to change weekly break point to Sunday
+## add a column that sets all month/day combinations to the same year
+v.Prices$date.common <- as.Date(paste0("2000-",format(v.Prices$date, "%j")), "%Y-%j")
+v.Prices$month.common <- as.Date(cut(v.Prices$date.common,breaks = "month"))
 
 # create a subset of rows based on conditions
 v.Prices.Wheat <- subset(v.Prices,commodity.1=="wheat" & unit.1=="kile" & commodity.2=="currency" & unit.2=="ops")
@@ -56,7 +63,7 @@ v.Prices.Wheat.Mean.Annual <- aggregate((quantity.2 + quantity.3)/2 ~ year, data
 v.Prices.Wheat.Min.Quarterly <- aggregate(quantity.2 ~ quarter, data=v.Prices.Wheat.Period, mean)
 v.Prices.Wheat.Max.Quarterly <- aggregate(quantity.3 ~ quarter, data=v.Prices.Wheat.Period, mean)
 ## merge the two data frames into one
-v.Prices.Wheat.Mean.Quarterly <- merge(v.Prices.Wheat.Min.Quarterly, v.Prices.Wheat.Max.Quarterly, by=c("quarter"))
+v.Prices.Wheat.Mean.Quarterly <- merge(v.Prices.Wheat.Min.Quarterly, v.Prices.Wheat.Max.Quarterly, by=c("quarter"), all=T)
 
 
 # plot
@@ -68,8 +75,8 @@ v.Plot.Base <- ggplot() +
   geom_segment(data = vFoodRiotsPeriod, aes(x = date, xend = date, y = 10, yend = 24, colour = "food riot"),
                size = 1, show.legend = F, na.rm = T, linetype=1)+ # linetypes: 1=solid, 2=dashed
   scale_x_date(breaks=date_breaks("2 years"), 
-               labels=date_format("%Y"),
-               limits=as.Date(c(v.Date.Start, v.Date.Stop))) + # if plotting more than one graph, it is helpful to provide the same limits for each
+               labels=date_format("%Y"))+
+               #limits=as.Date(c(v.Date.Start, v.Date.Stop))) + # if plotting more than one graph, it is helpful to provide the same limits for each
   theme_bw()+ # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
   theme(axis.text.x = element_text(angle = 45, vjust=0.5,hjust = 0.5, size = 8))  # rotate x axis text
 v.Plot.Base  
@@ -95,24 +102,28 @@ v.Plot.Wheat.Scatter <- v.Plot.Base+
 v.Plot.Wheat.Scatter
 
 ## quarterly averages
-
 v.Plot.Wheat.Quarterly.Mean.Scatter <- v.Plot.Base+
   # add labels
   labs(title="Wheat prices in Bilad al-Sham", 
-       subtitle="based on announcements in newspapers", 
-       #x="Date", 
+       subtitle="Quarterly averages of minimum and maximum prices based on announcements in newspapers", 
        y="Prices (piaster/kile)") +
   # first layer: min prices
-  geom_line(data = v.Prices.Wheat.Min.Quarterly, 
+  geom_point(data = v.Prices.Wheat.Mean.Quarterly, 
              aes(x = quarter, # select period: date, year, quarter, month
                  y = quantity.2),
-             na.rm=TRUE, color="green", size=2, pch=3)+
+             na.rm=TRUE, size=2, pch=3)+
+  scale_color_gradient(low="darkkhaki", high="darkgreen")+
+  #geom_text(data = v.Prices.Wheat.Mean.Quarterly,aes(x = quarter, quantity.2, label=round(quantity.2)), size=3)+
   # second layer: max prices
-  geom_line(data = v.Prices.Wheat.Max.Quarterly, 
+  geom_point(data = v.Prices.Wheat.Mean.Quarterly, 
              aes(x=quarter, y=quantity.3),
-             na.rm=TRUE, size=2, pch=3, color="red")
+             na.rm=TRUE, size=2, pch=3, color="black")+
   # layer with connecting lines between min and max prices
-
+  geom_segment(data = v.Prices.Wheat.Mean.Quarterly, 
+               aes(x = quarter, xend = quarter, 
+                   y = quantity.2, yend = quantity.3),
+               size = 0.3, show.legend = F, na.rm = T, linetype=1) # linetypes: 1=solid, 2=dashed,
+  
 v.Plot.Wheat.Quarterly.Mean.Scatter
   
 
@@ -153,6 +164,8 @@ v.Plot.Wheat.Box <- v.Plot.Base+
   #geom_boxplot(data = vWheatKilePeriod,aes(x=year,group=year,y=(quantity.2 + quantity.3) / 2), na.rm = T)+
   # layer: box plot min prices
   geom_boxplot(data = v.Prices.Wheat.Period,aes(x=year,group=year,y=quantity.2), na.rm = T)+
+  ## add error bars
+  stat_boxplot(geom ='errorbar')+
   # layer: box plot max prices
   geom_boxplot(data = v.Prices.Wheat.Period,aes(x=year, group=year,y=quantity.3), na.rm = T, color="blue", width=100)
   # layer: jitter plot
@@ -232,3 +245,34 @@ plotWheatKilePeriod3 <- ggplot(vWheatKilePeriod) +
   theme_bw()+ # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
   theme(axis.text.x = element_text(angle = 45, vjust=0.5,hjust = 0.5, size = 8))  # rotate x axis text
 plotWheatKilePeriod3
+
+
+# plot day of the year / explore seasonality
+
+v.Plot.Wheat.Annual.Cycle.Scatter <- ggplot()+
+  geom_point(data = v.Prices.Wheat.Period, 
+             aes(x = date.common, y = quantity.2),
+             na.rm=TRUE, size=2, pch=3, color="black")+
+  geom_point(data = v.Prices.Wheat.Period, 
+             aes(x = date.common, y = quantity.3),
+             na.rm=TRUE, size=2, pch=3, color="black")+
+  scale_x_date(breaks=date_breaks("1 month"), 
+               labels=date_format("%d-%b")) + # if plotting more than one graph, it is helpful to provide the same limits for each
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, vjust=0.5,hjust = 0.5, size = 8))  # rotate x axis text
+v.Plot.Wheat.Annual.Cycle.Scatter
+
+v.Plot.Wheat.Annual.Cycle.Box <- ggplot()+
+  # it would be important to know how many values are represented in each box
+  geom_boxplot(data = v.Prices.Wheat.Period, 
+             aes(x = month.common, group = month.common, y = quantity.2),
+             na.rm=TRUE, color="black")+
+  geom_boxplot(data = v.Prices.Wheat.Period, 
+               aes(x = month.common, group = month.common, y = quantity.3),
+               na.rm=TRUE, width = 10,  color="blue")+
+  stat_boxplot(geom ="errorbar")+
+  scale_x_date(breaks=date_breaks("1 month"), 
+               labels=date_format("%B")) + # %B full month names; $b abbreviated month
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, vjust=0.5,hjust = 0.5, size = 8))  # rotate x axis text
+v.Plot.Wheat.Annual.Cycle.Box
